1a3b29fb4decf09100ce4062869002f6a1f3ca41
Kenefiss <kenefiss@gmail.com>
2022-09-02 12:06:39 +0300

    added folders

---
+++ app/index.php
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+Hello world
+=======
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
+>>>>>>> 52271721a7e81cf9b8c79f8f5ea6d11e35600657
