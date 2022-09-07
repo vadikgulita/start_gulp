5cbcb6eae5131906591984e34b01296410dd49f6
Olha Vengrynovych <havdo.olia@gmail.com>
2022-09-07 17:50:34 +0300

    add style-mobile.css

--- build/_bottom.php
+++ build/_bottom.php
@@ -1,5 +1,7 @@
 <link href="css/swiper-bundle.min.css" rel="stylesheet" type="text/css" />
 <link href="css/style.css" rel="stylesheet" type="text/css" />
+<link href="css/style-mobile.css" rel="stylesheet" type="text/css" />
+
 
 <script defer src="js/jquery-3.6.1.min.js"></script>
 <script defer src="js/swiper-bundle.min.js"></script>
