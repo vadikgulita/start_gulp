6561ee863d4837af6bbe1dd35d4d44d3738faf85
Vadym Gulita <47484831+vadikgulita@users.noreply.github.com>
2022-09-08 10:47:31 +0300

    Add structure of index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <?php include 'inc/_top.php';?>
+   <title>The Name of the Site :: Home page</title>
+</head>
+
+<body>
+   <!-- LOADER -->
+   <div id="loader-wrapper"></div>
+
+   <div id="content-block">
+      <!-- HEADER -->
+      <?php include 'inc/_header.php';?>
+
+      <!-- CONTENT -->
+      <main>
+
+      </main>
+
+      <!-- FOOTER -->
+      <footer>
+         <?php include 'inc/_footer.php';?>
+      </footer>
+
+   </div>
+
+   <?php include 'inc/_bottom.php';?>
+</body>
+
+</html>
